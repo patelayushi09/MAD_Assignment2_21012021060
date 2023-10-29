3d69264a2202f72e4c50210fdab568df4d65a192
patelayushi09 <ashapatel99246@gmail.com>
2023-10-29 15:02:55 +0530

    main activity

--- app/src/main/java/com/example/mad_assignment2_21012021060/MainActivity.kt
+++ app/src/main/java/com/example/mad_assignment2_21012021060/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.mad_assignment2_21012021060
 
+/*
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var sqLiteHelper: SQLiteHelper
+    private var taskList: MutableList<TaskItem> = ArrayList()
+    private lateinit var adapter: TaskRecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnAddTask : Button = findViewById(R.id.addTaskBtn)
+        val taskRecyclerView : RecyclerView = findViewById(R.id.taskRecycler)
+
+        sqLiteHelper = SQLiteHelper(this)
+        taskList = sqLiteHelper.getTasks()
+
+        taskRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter =TaskRecyclerView(taskList)
+        taskRecyclerView.adapter = adapter
+
+
+        btnAddTask.setOnClickListener {
+            startActivity(Intent(this@MainActivity,AddTaskActivity::class.java))
+        }
     }
+
 }
+
+ */
